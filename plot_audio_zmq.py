ece7f3c76f269ee458c32177d463864ba6bff426
Luke Robison <lukerobison@gmail.com>
2014-10-05 14:55:51 -0500

    Updates to plot_audio_zmq.py
    It is now almost usable :-)

--- plot_audio_zmq.py
+++ plot_audio_zmq.py
@@ -5,6 +5,7 @@ import pylab
 import matplotlib.animation as animation
 import time
 import threading
+import numpy as np
 
 
 class ZMQAudioRead:
@@ -19,13 +20,18 @@ class ZMQAudioRead:
 
     def read_more(self):
         dat_str = True
+        dtbuf = np.dtype('h')
+        dtfc  = np.dtype('I')
         while dat_str:
-            print "reading..."
+            #print "reading..."
             mpart = self.socket.recv_multipart()
             dat_hdr = mpart[0]
-            dat_str = mpart[1]
-            audio = array.array('H',dat_str)
-            yield audio
+            dat_framenum = mpart[1]
+            dat_str = mpart[2]
+            audio = np.frombuffer(dat_str,dtype=dtbuf)
+            frame = np.frombuffer(dat_framenum,dtype=dtfc)
+            
+            yield (audio,frame[0])
         print "done reading"
             
 
@@ -35,34 +41,43 @@ class AudioPlotter:
         self.fig,self.ax = pylab.subplots()
         self.line, = self.ax.plot([],[],lw=2)
         self.datgen = self.datsrc.read_more()
-        self.audio = []
+        self.audio = np.zeros(8000,dtype=np.dtype('h'))
+        self.frame_counter = 0
 
         self.thread = threading.Thread(target=self.update_data)
 
     def update_data(self):
         while True:
-            self.audio = self.datgen.next()
+            (new_audio,self.frame_counter) = self.datgen.next()
+            self.audio = np.roll(self.audio, -len(new_audio))
+            start = len(self.audio) - len(new_audio)
+            stop = len(self.audio)
+            self.audio.put(np.arange(start,stop), new_audio)
     
     def update_screen(self):
         self.line.set_data(range(0,len(self.audio)),self.audio)
+        old_lim = self.ax.get_ylim()
+        old_lim = max(old_lim)*1.25
+        data_lim = abs(self.audio).max()
+        new_lim = 2*np.mean([0.7*old_lim, 0.3*data_lim])
+        new_lim = max([1, new_lim])
+        #if self.frame_counter%100 == 1:
         self.ax.set_xlim(0,len(self.audio))
-        self.ax.set_ylim(min(self.audio),max(self.audio))
+        self.ax.set_ylim(-new_lim,new_lim)
+            #print "min/max: %d/%d"%(min(self.audio),max(self.audio))
         return self.line
 
     def main(self):
         # start getting data
         self.thread.start()
 
-        while not self.audio:
-            print "waiting for first data..."
-            time.sleep(0.25)
-        print "got data! (mainloop)"
+        
         while True:
             self.update_screen()
             pylab.draw()
             pylab.show()
-            pylab.pause(0.25)
-            print "draw loop done!"
+            pylab.pause(0.01)
+            #print "draw loop done!"
 
     
 #    from multiprocessing import Process
