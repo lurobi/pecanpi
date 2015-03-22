cd9841450d6b8d11ed140bb2e983d82baf2a064b
Bob Robison <bob@robisonranch.net>
2015-03-22 14:25:23 -0500

    Merging other changes?

--- azmqMon.py
+++ azmqMon.py
@@ -4,6 +4,10 @@
 # @file   azmqMon.py
 # @brief  Monitor audio stream sent via ZMQ
 # @author Bob Robison
+# 
+# v1.1.0 - 21 Mar 2014 -- lar
+#     Added SpecExtents class
+# v1.0.0 - 30 Sep 2014
 ###############################################################################
 #   DESCRIPTION: 
 '''
@@ -18,15 +22,67 @@ import numpy.fft as npfft
 import numpy
 import curses
 
-VERSION = '1.0.0'
-VERSDATE = '30 Sep 2014'
-############################################################################
-## ZMQAudioRead
-############################################################################
+VERSION = '1.1.0'
+VERSDATE = '21 Mar 2015'
 def dB20(lin_val):
   if lin_val==0: return -numpy.inf
   else: return 20*math.log10(abs(lin_val))
 
+# SpecExtents class holds info on screen pan/zoom/setup
+#   Initialize with a copy of the curses window class
+class SpecExtents:
+  def __init__(self,win,fs=8000.0):
+    self.win=win
+    self.fs=fs
+    (self.maxy,self.maxx)=self.win.getmaxyx()
+    self.curPos=self.maxx/2
+    self.loMag=-50.0
+    self.hiMag=+5.0
+    self.magPos=self.maxy/2
+    self.magPosdB=(self.hiMag - self.loMag)/2 + self.loMag
+    self.loHz=0
+    self.hiHz=self.fs/2
+    self.hzPerPixel=(self.fs/2) / double(self.maxx)
+    self.curPosHz=self.curPos*self.hzPerPixel + self.loHz
+  # adjustExtents units are w/respect to 1.0 = 100%
+  #  i.e. panx 1.0 does nothing, panx=1.5 pans by 50% of current x range
+  #       similar for pany 
+  #       zoomx or zoomy multiplies current zoom factor, zoom>1 zoomin, zoom<1 zoom out, zoom=0 resets to full zoom
+  def adjustExtents(self,panx,pany,zoomx,zoomy):
+    if zoomx==0:
+      self.zoomx = 1.0
+    else:
+      self.zoomx *= zoomx
+    if zoomy==0:
+      self.zoomy = 1.0
+    else:
+      self.zoomy *= zoomy
+    # Adjust for zoom first
+
+
+    # Adjust pan
+    #  X
+    curxrange=(self.hiHz - self.loHz)
+    panxamt=curxrange*panx
+    self.loHz += panxamt
+    self.hiHz += panxamt
+    if self.loHz < 0:
+      self.loHz=0
+    if self.hiHz > self.fs/2:
+      self.hiHz=self.fs/2
+    if self.hiHz < self.loHz:
+      self.hiHz = self.loHz+1
+    #  Y
+    curyrange=(self.hiMag - self.loMag)
+    panyamt=curyrange*pany
+    self.loMag += panyamt
+    self.hiMag += panyamt
+    if self.loMag < -50:
+      self.loMag=-50
+    if self.hiMag > 5:
+      self.hiMag=5
+    if self.hiMag < self.loMag:
+      self.hiMag = self.loMag+1
 ############################################################################
 ## App class
 ##   Top-level application, based on SigExpCmdApp
@@ -40,6 +96,7 @@ class App(PyCmdApp):
     self.done=False
     self.logfile=None
     self.fs=8000
+    self.logwinOut={}
     self.ipaddr="127.0.0.1"
     self.ipport=5563
 
@@ -108,24 +165,30 @@ class App(PyCmdApp):
       self.curwin.addstr(0,self.curPos,"^",7)
       if self.lastzoom != self.zoom:  # Redo scale
         self.lastzoom=self.zoom
-        self.loXkHz = (self.curPoskHz - self.fs/(2e3*self.zoom)) 
-        self.hiXkHz = (self.curPoskHz + self.fs/(2e3*self.zoom)) 
+        self.loXkHz = (self.curPoskHz - self.fs/(4e3*self.zoom)) 
+        self.hiXkHz = (self.curPoskHz + self.fs/(4e3*self.zoom)) 
+        self.logMsg("Candidate Zoom level Ctr: %.1f ->%.1f to %.3f -> %.3f\n"%\
+                    ( self.curPoskHz,self.zoom,self.loXkHz,self.hiXkHz))
         if self.loXkHz < 0:
           self.loXkHz = 0
         if self.hiXkHz > self.fs/2e3:
           self.hiXkHz = self.fs/2e3
+        self.curPos=(maxx/2)
+        self.curPoskHz=(self.loXkHz + self.hiXkHz)/2 + self.loXkHz
+        self.logMsg("Actual Zoom level %.1f to %.3f -> %.3f\n"%(self.zoom,self.loXkHz,self.hiXkHz))
       self.curwin.noutrefresh()
+      self.drawXAxis()
     except Exception,ex:
       self.logMsg( "Got exception: %s\n"%ex)
       self.OnExit()
   def drawXAxis(self):
     (maxy, maxx) = self.xaxiswin.getmaxyx()
     self.xaxiswin.clear()
-    xstart_str = '%gkHz'%(self.loXkHz)
-    xstop_str = '%gkHz'%(self.hiXkHz)
-    xhalf_str = '%gkHz'%((self.hiXkHz+self.loXkHz)/2.0)
-    x1quar_str  = '%gkHz'%((self.hiXkHz-self.loXkHz)/4.0 + self.loXkHz)
-    x3quar_str = '%gkHz'%(3.0*(self.hiXkHz-self.loXkHz)/4.0 + self.loXkHz)
+    xstart_str = '%.3fkHz'%(self.loXkHz)
+    xstop_str = '%.3fkHz'%(self.hiXkHz)
+    xhalf_str = '%.3fkHz'%((self.hiXkHz+self.loXkHz)/2.0)
+    x1quar_str  = '%.3fkHz'%((self.hiXkHz-self.loXkHz)/4.0 + self.loXkHz)
+    x3quar_str = '%.3fkHz'%(3.0*(self.hiXkHz-self.loXkHz)/4.0 + self.loXkHz)
     self.xaxiswin.addstr(0, 0,xstart_str)
     self.xaxiswin.addstr(0, maxx/4-len(x1quar_str)/2,x1quar_str)
     self.xaxiswin.addstr(0, maxx/2-len(xhalf_str)/2,xhalf_str)
@@ -140,6 +203,8 @@ class App(PyCmdApp):
     pcolor=0
     bingroup=len(mag)/(float(maxx)*2*self.zoom)
     bingroup=bingroup
+    peakVal=-200
+    peakIdx=0
     for ix in range(0,maxx):
       lobin=int(ix*bingroup)
       hibin=int((ix+1)*bingroup)
@@ -148,6 +213,11 @@ class App(PyCmdApp):
         if grpx < len(mag):
           if mag[grpx]>binval:
             binval=mag[grpx]
+            if binval > peakVal:
+              peakVal=binval
+              xkhz= (ix/float(maxx))* (self.hiXkHz - self.loXkHz) + self.loXkHz
+              self.logwinOut[2]="bin %d-%d (ix=%d) Peak=%.1f @ %.3f kHz"%\
+                (lobin,hibin,ix,binval,xkhz)
       # Now have the value to plot in this column
       loEnd=-50.0
       hiEnd=+5.0
@@ -157,12 +227,15 @@ class App(PyCmdApp):
         yval=maxy
       else:
         yval = int(maxy - ((binval-loEnd)/(hiEnd-loEnd) * maxy))
-      self.verboseLogMsg("Bin [%d-%d] %d => %.1f => %d\n"%(lobin,hibin,ix,binval,yval))
+      if binval > -20:
+        self.logMsg("Bin [%d-%d] %d => %.1f => %d\n"%(lobin,hibin,ix,binval,yval))
       for yx in range(yval,maxy-1):
         self.specwin.addstr(yx,ix,'#',pcolor)
     if not self.done:
       self.specwin.refresh()
   def analyze(self):
+    (maxx,maxy)=self.specwin.getmaxyx()
+    self.logwinOut[9]="maxx=%d, maxy=%d"%(maxx,maxy)
     if len(self.audio)>0:
       obuf=npfft.fft(self.audio,2048)
       mag=map(lambda x: dB20(x) - 147,obuf)
@@ -172,16 +245,26 @@ class App(PyCmdApp):
       frqval=float(maxidx)*fs/len(obuf)
       self.statstr="Count: %d, min: %5d, max: %5d, obuf: %.1f @ %.1f Hz        "%\
         (self.count,self.audio[0]+min(self.audio),max(self.audio),maxmag,frqval)
+      self.logwinOut[1]=self.statstr
       self.drawSpec(mag)
   def getCursorInfo(self):
     rval="Zoom: %.1f, curPos: %d, curPoskHz: %.3f, lofreq: %.3f kHz, hifreq= %.3f kHz"%\
       (self.zoom,self.curPos,self.curPoskHz,self.loXkHz,self.hiXkHz)
     return rval
+  def getLogInfo(self,ix):
+    if ix in self.logwinOut:
+      return self.logwinOut[ix]
+    else:
+      return "No info for line %d"%ix
   def updateStatView(self):
     self.analyze()
+    (maxy, maxx) = self.logwin.getmaxyx()
     if not self.done:
-      self.logwin.addnstr(2,0,self.getCursorInfo(),self.maxx)
-      self.logwin.addnstr(3,0,self.statstr,self.maxx)
+      self.logwinOut[0]=self.getCursorInfo()
+      for ix in range(0,maxy):
+        self.logwin.addnstr(ix,0,self.getLogInfo(ix),self.maxx)
+      #self.logwin.addnstr(ix,0,self.getCursorInfo(),self.maxx)
+      #self.logwin.addnstr(3,0,self.statstr,self.maxx)
       self.logwin.refresh()
   def update_data(self):
     while not self.done:
@@ -252,11 +335,9 @@ class App(PyCmdApp):
          elif(event == 260): # left arrow
              self.curPos-=1 # move cursor to the left
              self.curPos=max(0,self.curPos)
-             self.curPoskHz= (self.curPos/float(maxx))* (self.hiXkHz - self.loXkHz) + self.loXkHz
          elif(event == 261): # right arrow
              self.curPos+=1 # move cursor to the right
              self.curPos=min(maxx,self.curPos)
-             self.curPoskHz= (self.curPos/float(maxx))* (self.hiXkHz - self.loXkHz) + self.loXkHz
          elif(event == 259): # up arrow
            self.zoom = self.zoom*0.8
            if self.zoom<1.0:
@@ -265,6 +346,8 @@ class App(PyCmdApp):
            self.zoom = self.zoom*1.2
            if self.zoom>50:
              self.zoom=50
+         self.curPoskHz= (self.curPos/float(maxx))* (self.hiXkHz - self.loXkHz) + self.loXkHz
+         self.hzperpixel=(self.fs/2)/(double(maxx)*self.zoom)
          self.updateCursor()
 	 curses.doupdate()
          # shows over, tear down
